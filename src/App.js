3704f0017dda34d47788efd11d8c0e2fc425a210
dipeshkoirala <dipeshkoirala@gmail.com>
2020-04-29 22:10:59 -0400

    data wrangling

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
         !
       </p>
       <Npod />
-      {/* It has to be green */}
     </div>
   );
 }
--- src/component/NpodData.js
+++ src/component/NpodData.js
@@ -3,41 +3,24 @@ import axios from "axios";
 
 import Npod from "./Npod";
 
-export default function MovList() {
-  const [mov, setMov] = useState([]);
-
-  useEffect(() => {
+export default function testList() {
+  const [text, setTest] = useState();
+  
     axios
-      .get(
-        "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2020-04-28"
-      )
+      .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-20`)
       .then((response) => {
-        console.log(response);
-        setMov(response.data);
+        console.log(response.data);
+        setTest(response);
       })
       .catch((err) => {
         console.log("couldn;t get the resuelt" + err);
       });
-  }, []);
+  };
 
-  // call an web API, and get a list of movies
-  // https://ghibliapi.herokuapp.com/films
   return (
     <div className="photo">
-      <ul>
-        {/* {mov.map((mo) => {
-          return (
-            <Npod
-            // id={mo.id}
-            // val={mo.title}
-            // description={mo.description}
-            // director={mo.director}
-            // release_date={mo.release_date}
-            />
-          ); */}
-        })}
-      </ul>
+      <Npod item1={text} />
+      <ul></ul>
     </div>
   );
-  // render the movies to the screen using the MovieCard component for each movie.
 }
